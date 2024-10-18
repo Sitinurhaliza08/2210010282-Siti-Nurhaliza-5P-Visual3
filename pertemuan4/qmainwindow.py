d7d40d3e42d818c35685a18a22c57b3d819146f7
Sitinurhaliza08 <nurhaliza180804@gmail.com>
2024-10-18 19:10:09 +0800

    Update and rename mainwindow.py to qmainwindow.py

--- pertemuan4/qmainwindow.py
+++ pertemuan4/qmainwindow.py
@@ -1,13 +1,16 @@
 # ================= Menggunakan QMianwindow ========================
 from PyQt5.QtWidgets import QApplication, QPushButton, QLabel, QMainWindow
+
 app = QApplication([])
 
 window = QMainWindow()
-label = QLabel(window)
+#label = QLabel("Label", window) # cara 1
+label = QLabel(window)           # cara 2
 label.setText("Label1")
 label.move(200,0)
 
-button = QPushButton(window)
+#button = QPushButton("MyButton", window) # cara 1
+button = QPushButton(window)              # cara 2
 button.setText("Button1")
 
 window.show()
