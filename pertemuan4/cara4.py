0841292fc03548ea9399a393d1b241725cf20c4e
Sitinurhaliza08 <nurhaliza180804@gmail.com>
2024-10-18 19:21:30 +0800

    Delete pertemuan4/cara4.py

--- pertemuan4/cara4.py
+++
@@ -1,8 +0,0 @@
-# ============== Cara 4 (recommended, lebih hemat memori)  ============
-
-from PyQt5.QtWidgets import QApplication, QPushButton
-
-app = QApplication([])
-window = QPushButton("MyButton")
-window.show()
-app.exec_()
